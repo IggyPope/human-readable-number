cdd50a989b8b323ed9eaa55ea3b1fce7a649ff59
IggyPope <altko4egar@gmail.com>
2023-09-18 01:36:47 +0300

    feat: implement toReadable(number) function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const digits = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const thousands = ["", "thousand", "million", "billion", "trillion"];
+
+    if (number === 0) {
+        return "zero";
+    } else if (number < 20) {
+        return digits[number];
+    }
+
+    const inputArray = number.toString().split("").reverse();
+
+    const resultArray = inputArray.reduce((acc, cur, i, arr) => {
+        if (i % 3 === 0) {
+            acc.unshift((digits[+cur] + " " + thousands[i / 3]).trim());
+        } else if (i % 3 === 1) {
+            if (cur === "1") {
+                acc.splice(0, 1, digits[cur + arr[i - 1]]);
+            } else {
+                acc.unshift(tens[+cur]);
+            }
+        } else if (i % 3 === 2) {
+            acc.unshift(digits[+cur] + " hundred");
+        }
+        return acc;
+    }, []);
+
+    return resultArray
+        .filter((el) => el !== "")
+        .join(" ")
+        .trim();
+};
